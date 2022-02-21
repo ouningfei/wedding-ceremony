c7f585640bc4498238bebf94d93ef2732b774dab
Ou Ningfei <ouningfei@gmail.com>
2022-02-21 11:29:54 +0800

    clean up assets moved pics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
-import lizning from './lizning.jpg';
-import lizning2 from './lizning2.jpg';
-import pic2 from './pic2.jpg';
-import pic3 from './pic3.jpg';
-import pic4 from './pic4.jpg';
-import pic5 from './pic5.jpg';
-import pic6 from './pic6.jpg';
-import pic7 from './pic7.jpg';
-import pic8 from './pic8.jpg';
-import pic9 from './pic9.jpg';
-import pic10 from './pic10.jpg';
-import Marquee from "./Marquee";
 import './App.css';
 import './Components/Grid.js';
-import CenteredGrid from './Components/Grid.js';
-import SpotifyPlayer from 'react-spotify-player';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
-import Home from './Components/home.js';
-import About from './Components/about.js';
+import Portfolio from './Components/portfolio.js';
 import Contact from './Components/contact.js';
 import Wedding from './Components/wedding.js';
 // import { Typography } from '@material-ui/core';
@@ -35,7 +20,7 @@ render() {
     <Router>
            <Routes>
                  <Route path='/' element={< Wedding />}></Route>
-                 <Route path='/about' element={< About />}></Route>
+                 <Route path='/portfolio' element={< Portfolio />}></Route>
                  <Route path='/contact' element={< Contact />}></Route>
           </Routes>
     </Router>
---
+++ src/Components/portfolio.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Portfolio () {
+	return <div>
+	<iframe src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FDTigwst0hMaW48m1Vm0ndY%2FPortfolio-Website" width="100%" height="1000" position="absolute" top="0" left="0" title="Figma Portfolio" allowfullscreen></iframe>
+	</div>
+}
+export default Portfolio;
