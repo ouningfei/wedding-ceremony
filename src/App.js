cfa3121cf2636e8e4e83341d8b39afa8c906d621
Ou Ningfei <ouningfei@gmail.com>
2022-02-21 11:45:20 +0800

    addressed build errors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import './Components/Grid.js';
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router,Routes, Route, } from 'react-router-dom';
 import Portfolio from './Components/portfolio.js';
 import Contact from './Components/contact.js';
 import Wedding from './Components/wedding.js';
 // import { Typography } from '@material-ui/core';
-
-const size = {
-  width: '30%',
-  height: 90,
-};
-const view = 'list'; // or 'coverart'
-const theme = 'black'; // or 'white'
 
 class App extends Component {
 render() {
