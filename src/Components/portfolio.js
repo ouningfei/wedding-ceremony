55fd264d2d662728e321b0ff726b0fa281b2b703
Ou Ningfei <ouningfei@gmail.com>
2022-02-21 14:41:09 +0800

    remove hotspot, new link by liz

--- src/Components/portfolio.js
+++ src/Components/portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Portfolio () {
 	return <div>
-	<iframe src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FDTigwst0hMaW48m1Vm0ndY%2FPortfolio-Website" width="100%" height="1000" position="absolute" top="0" left="0" title="Figma Portfolio" allowfullscreen></iframe>
+	<iframe src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FDTigwst0hMaW48m1Vm0ndY%2FPortfolio-Website%3Fpage-id%3D1%253A3%26node-id%3D159%253A252%26viewport%3D318%252C48%252C1.61%26scaling%3Dmin-zoom%26starting-point-node-id%3D159%253A252%26hotspot-hints%3D0" width="100%" height="1000" position="absolute" top="0" left="0" title="Figma Portfolio" allowfullscreen></iframe>
 	</div>
 }
 export default Portfolio;
